05bf7599ec7d9ee2f1e6fcf6bb4620cf39c11d48
Nem <nguyennamclone69@gmail.com>
2025-07-04 23:48:26 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Lặp lại hiệu ứng sáng tối của trang web
-    setInterval(function() {
-        document.body.classList.toggle('dark-mode');
-    }, 3000); // Chuyển đổi hiệu ứng sáng tối mỗi 3 giây
-});
